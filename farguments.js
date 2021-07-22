86fe903ac3737ac34e67e6b8cc237a688a784aff
Erico103 <85289051+Erico103@users.noreply.github.com>
2021-07-22 15:57:48 +0200

    Add files via upload

---
+++ farguments.js
@@ -0,0 +1,14 @@
+const paintWall = function (color){
+    console.log("The wall has painted " + color)
+
+}
+
+paintWall("green");
+paintWall("red")
+
+const paintWall2 = function(wallLocation, color){
+console.log('The '+wallLocation+' has been painted '+color)
+}
+
+paintWall2('North wall', 'orange')
+paintWall2('South-East wall', 'grey')
